94de8454e88328f578956a0a1f78353d2f0cf040
siva585leela <siva585leela@gmail.com>
2023-04-13 01:15:42 +0530

    First commit Accessing Minio Copying Local Folders to Minio Server

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from minio import Minio
+
+# Initialize minIO client
+client = Minio(
+    "localhost:9000",  # Replace with your minIO server address
+    access_key="minioadmin",  # Replace with your access key
+    secret_key="minioadmin",  # Replace with your secret key
+    secure=False,  # Set to True for SSL/TLS encrypted connections
+)
+
+# Set the path of the local folder to upload
+local_folder_path = "C:\\MyLocalFolder"
+
+# Set the name of the minIO bucket to upload to
+bucket_name = "mybucket"
+
+# Create the bucket if it doesn't exist
+if not client.bucket_exists(bucket_name):
+    client.make_bucket(bucket_name)
+
+# Iterate over the files in the local folder and upload them to the minIO bucket
+for root, dirs, files in os.walk(local_folder_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        object_name = os.path.relpath(file_path, local_folder_path).replace("\\", "/")
+        client.fput_object(bucket_name, object_name, file_path)
+
+print("Folder uploaded successfully!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
